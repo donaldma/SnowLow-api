d3bd50f5121fe649234bde744fbac493d78ee5f4
donaldma <donald.ma@hotmail.ca>
2018-03-13 21:43:08 -0700

    add protection

--- src/controllers/BotController.ts
+++ src/controllers/BotController.ts
@@ -1,11 +1,17 @@
 'use strict'
 
 import BotService from '../services/BotService'
+import { NotAuthorized } from '../aws/DynamodbResponse'
 
 module.exports.movieBot = async (event, context, callback) => {
   await BotService.checkTickets(event.pathParameters, callback)
 }
 
 module.exports.pcPriceBot = async (event, context, callback) => {
+  if(event.headers.token !== process.env.AUTH_TOKEN) {
+    callback(null, NotAuthorized({ status: false, error: 'Sorry not authorized, foh b' }))
+    return
+  }
+
   await BotService.checkPrice(event.pathParameters, callback)
 }
