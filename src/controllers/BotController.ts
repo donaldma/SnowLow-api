6973e1ee0c839384b54bdfcf8f4f2da3e72af207
donaldma <donald.ma@hotmail.ca>
2018-03-13 21:46:38 -0700

    protect ticket bot

--- src/controllers/BotController.ts
+++ src/controllers/BotController.ts
@@ -4,6 +4,11 @@ import BotService from '../services/BotService'
 import { NotAuthorized } from '../aws/DynamodbResponse'
 
 module.exports.movieBot = async (event, context, callback) => {
+  if(event.headers.token !== process.env.AUTH_TOKEN) {
+    callback(null, NotAuthorized({ status: false, error: 'Sorry not authorized, foh b' }))
+    return
+  }
+
   await BotService.checkTickets(event.pathParameters, callback)
 }
 
