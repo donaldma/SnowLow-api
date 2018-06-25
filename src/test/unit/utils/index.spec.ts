e83c238290f305c1176b6733bf4a220a201b55dd
donaldma <donald.ma@hotmail.ca>
2018-06-25 11:48:16 -0700

    test var 5

--- src/test/unit/utils/index.spec.ts
+++ src/test/unit/utils/index.spec.ts
@@ -3,6 +3,9 @@ import 'mocha'
 describe('tests', () => {
   it('test', () => {
     console.log('process.env.TEST_TOKEN', process.env.TEST_TOKEN)
+    if(process.env.TEST === 'test') {
+      console.log('ok it works')
+    }
   })
 
 })
